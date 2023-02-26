395b1c3f1ff05032b405132d6eb1c44622dd33dd
JackHughes03 <kannsbusiness@yahoo.com>
2023-02-26 01:32:06 +0000

    14

--- scripts/index.js
+++ scripts/index.js
@@ -11,10 +11,10 @@ var navbuttonsback = document.getElementById("navbuttonsback");
 hamBurger.addEventListener('click', () => {
     // display navbuttons
     if(navbuttons[0].style.display === "block") {
-    for (var i = 0; i < navbuttons.length; i++) {
-        navbuttons[i].style.display = "none";
-    }
-    navbuttonsback.style.display = "none";
+        for (var i = 0; i < navbuttons.length; i++) {
+            navbuttons[i].style.display = "none";
+        }
+        navbuttonsback.style.display = "none";
     } else {
         for (var i = 0; i < navbuttons.length; i++) {
             navbuttons[i].style.display = "block";
@@ -33,3 +33,42 @@ function playpause() {
 
 var formHeight = document.querySelector('form').clientHeight;
 document.querySelector('#backgroundform').style.height = (formHeight + 50) + 'px';
+
+function changefr() {
+    // Define the translations from English to French
+    const translations = {
+        'Hello': 'Bonjour',
+        'Welcome': 'Bienvenue',
+        'Home': 'Accueil',
+        // Add more translations here
+    };
+    
+    // Loop through all elements in the document and replace their English text with French text
+    const elements = document.querySelectorAll('*');
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        const originalText = element.textContent;
+        const translatedText = translations[originalText];
+        if (translatedText) {
+            element.textContent = translatedText;
+        }
+    }
+}
+
+function changefr() {
+    var translate = document.getElementById("google_translate_element");
+
+    if (translate.style.display === "block") {
+        translate.style.display = "none";
+    }
+    else {
+        translate.style.display = "block";
+    }
+}
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement(
+        {pageLanguage: 'en'},
+        'google_translate_element'
+    );
+}
